4da5f35192fec17c2ed6ef7acb97881b4111958a
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2023-12-17 21:36:56 -0500

    cambios que hizo el sistema

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'cypress'
+//http://localhost:4200   
+//http://frontendabc-env.eba-txewrwr8.us-east-2.elasticbeanstalk.com/abc-jobs/en-US/
 
 export default defineConfig({
   
